2ca23a6e0f6ce055707d2fed3c50c823d399ddb2
Ibenye Jehoshaphat <jehoibenye@gmail.com>
2024-03-06 23:33:04 -0800

    jgug

---
+++ src/Content.js
@@ -0,0 +1,78 @@
+import {useState} from 'react';
+import {FaTrashAlt } from 'react-icons/fa';
+
+const Content = () => {
+    const [items, setItems] = useState([
+        {
+           id: 1,
+           checked: true,
+           item: "one half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: true,
+            item: "Item2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item3"
+        }
+      ]);
+
+      const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+      }
+    // const [count, setCount] = useState(0);
+    // const name = "Jeho"
+// const handleNameChange = () => {
+//   const names = ['Ebuka', 'James', 'Jeho', 'Onyinye'];
+//   const int = Math.floor(Math.random() * 4);
+//   setName(names[int]);
+// }
+
+// const handleClick = () => {
+//     setCount(count + 1)
+//     setCount(count + 1)
+//     console.log(count)
+// }
+ 
+// const handleClick2 = () => {
+//     console.log(count)
+// }
+
+// const handleClick3 = (e) => {
+//     console.log(e.target.innerText)
+// }
+
+  return (
+    <main>
+     {/* <p onDoubleClick={handleClick}>
+         Hello {name}! 
+    </p>
+    <button onClick={handleNameChange}>Change Name</button>       
+    <button onClick={handleClick}>Click It</button>       
+    <button onClick={handleClick2}>Click It</button>        */}
+        <ul>
+            {items.map((item) => (
+                <li className="item" key={item.id}>
+                    <input 
+                    type="checkbox"
+                    onChange={() => handleCheck(item.id)}
+                    checked = {item.checked}
+                    />
+                    <label htmlFor="">{item.item}</label>
+                    <FaTrashAlt 
+                        role="button" 
+                        tabIndex="0" 
+                     />
+
+                </li>
+            ))}
+        </ul>
+   </main>
+  )
+}
+
+export default Content
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    const headerStyle = {
+        backgroundColor: 'royalblue',
+        color:'#fff'
+    };
+
+  return (
+    <header style={headerStyle}>
+        <h1>Groceries list</h1>
+    </header>
+  )
+}
+
+export default Header;
